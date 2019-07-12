2f6c6c3b4347567e07e02be611028f6aea2238d4
shanmukha sri <shannmughasri@gmail.com>
2019-07-12 10:18:42 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='First project to try out cookie cutter.',
+    author='srikalidindi',
+    license='BSD-3',
+)
